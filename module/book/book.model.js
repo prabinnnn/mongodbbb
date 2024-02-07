f373fd2b672476741ddc4d66b0cddbd8406ab6a4
prabinnnn <prabinbhandari120@gmail.com>
2024-02-07 11:55:17 +0545

    add index

--- module/book/book.model.js
+++ module/book/book.model.js
@@ -1,6 +1,9 @@
 const { Schema, model } = require("mongoose");
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: string, required: true },
+  author: { type: String, required: true },
+  content: { type: String, required: true },
+  createdat: { type: String, required: true },
+  updateat: { type: String, required: true },
 });
 module.exports = new model("book", bookSchema);
---
+++ module/book/book.validator.js
@@ -0,0 +1,16 @@
+const Joi = require("joi");
+const schema = Joi.object({
+  title: Joi.string().required(),
+  author: Joi.string().required(),
+  content: Joi.string().required(),
+});
+const validate = (req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  } else {
+    next();
+  }
+};
+
+module.exports = { validate };
